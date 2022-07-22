d61dc891ccef7454d74d1792938d66c16dbade7e
Dhon23 <m.ramadhan.ak@gmail.com>
2022-07-22 09:23:44 +0700

    ke heroku

---
+++ PP/seeders/20220722021132-seedGuest.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const fs = require('fs');
+
+module.exports = {
+  up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     const data = JSON.parse(fs.readFileSync('./data/userGuest.json', 'utf-8'))
+     .map(el => {
+       return {
+         email: el.email,
+         password: el.password,
+         role: el.role,
+         createdAt : new Date(),
+         updatedAt : new Date()
+       }
+     });
+
+    return queryInterface.bulkInsert('Users', data)
+  },
+
+  down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     return queryInterface.bulkInsert('Users', null)
+  }
+};
